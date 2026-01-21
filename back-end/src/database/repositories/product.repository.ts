1f6c28f388be73f0fd88e9ad425950ed8373f075
leonardourci <leonardoguimaraesurci@gmail.com>
2026-01-20 23:14:17 -0300

    rollback getProductByExternalProductId repository function

--- back-end/src/database/repositories/product.repository.ts
+++ back-end/src/database/repositories/product.repository.ts
@@ -25,6 +25,30 @@ export const getProductById = async ({ id }: { id: string }): Promise<IProduct |
     }).toJSON()
 }
 
+export const getProductByExternalProductId = async ({ id }: { id: string }): Promise<IProduct | null> => {
+    const [row] = await knex(Product.tableName)
+        .where({ external_product_id: id })
+        .select()
+
+    if (!row) {
+        return null
+    }
+
+    return new Product({
+        id: row.id,
+        name: row.name,
+        currency: row.currency,
+        description: row.description,
+        price: row.price,
+        type: row.type,
+        externalProductId: row.external_product_id,
+        externalPriceId: row.external_price_id || '',
+        active: row.active,
+        createdAt: row.created_at,
+        updatedAt: row.updated_at
+    }).toJSON()
+}
+
 export const getAllProducts = async (): Promise<IProduct[]> => {
     const rows = await knex(Product.tableName)
         .where({ active: true })

453eb2edfbefb555c3608c630d109b80b77f598d
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2026-01-27 13:38:18 -0300

    Add locale prop to GoogleOAuthProvider for consistent language setting

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -9,7 +9,10 @@ const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <GoogleOAuthProvider clientId={googleClientId}>
+        <GoogleOAuthProvider
+            clientId={googleClientId}
+            locale='en'
+        >
             <TRPCProvider>
                 <App />
             </TRPCProvider>
